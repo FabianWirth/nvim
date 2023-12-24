22f4a7e13b64c5ec6c95fe8f4505bd4b67f5a472
Fabian Wirth <fabian-wirth@outlook.com>
2023-12-24 13:59:14 +0100

    added neovide config for transparency

--- init.lua
+++ init.lua
@@ -12,3 +12,17 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("fabianwirth")
 require("finisher")
+
+local alpha = function()
+				local trans = vim.g.transparency or 0.8
+				return string.format("%x", math.floor(255 * trans))
+end
+
+
+-- neovide configuration
+if vim.g.neovide then
+				vim.g.neovide_transparency = 0.8
+				vim.g.transparency = 0.8
+				vim.g.neovide_background_color = "#000fff"..alpha()
+end
+
